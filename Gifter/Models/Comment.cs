66cb6ff6d6bbda8d61c61c5e27ca2867c30424a0
Austin Ring <aring97@gmail.com>
2021-01-11 14:32:49 -0600

    finished

---
+++ Gifter/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gifter.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        [Required]
+        public int UserProfileId { get; set; }
+        [Required]
+        public int PostId { get; set; }
+        [Required]
+        public string Message { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public Post Post { get; set; }
+    }
+}
---
+++ Gifter/Repositories/CommentRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Gifter.Data;
+using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public List<Comment> GetAll()
+        {
+            return _context.Comment.Include(c => c.UserProfile).Include(c => c.Post).ToList();
+        }
+        public Comment GetById(int id)
+        {
+            return _context.Comment.Include(c => c.UserProfile).Include(c => c.Post).FirstOrDefault(c => c.Id == id);
+        }
+        public List<Comment> GetByPostId(int postId)
+        {
+            return _context.Comment.Include(c => c.UserProfile).Include(c => c.Post)
+                    .Where(c => c.PostId == postId).ToList();
+        }
+        public void Add(Comment comment)
+        {
+            _context.Add(comment);
+            _context.SaveChanges();
+        }
+        public void Update(Comment comment)
+        {
+            _context.Entry(comment).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var comment = GetById(id);
+            _context.Comment.Remove(comment);
+            _context.SaveChanges();
+        }
+    }
+}
